5621044cbcb345f189901ebc205921226e92372f
romiimartinez <rominamacarena.martinezpriore@everis.nttdata.com>
2022-01-05 16:56:01 -0300

    fix error

--- TicTacToe/View/MainWindow.xaml.cs
+++ TicTacToe/View/MainWindow.xaml.cs
@@ -1,7 +1,19 @@
-﻿using System.Windows;
-using TicTacToe.Models;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
 
-namespace View
+namespace GUI
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -12,6 +24,5 @@ namespace View
         {
             InitializeComponent();
         }
-
-}
+    }
 }
